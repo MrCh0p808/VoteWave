80822974ad8d7ff25e905064c4d07d1f665da8fe
MrCh0p808 <trideevg7@gmail.com>
2025-09-10 05:55:37 +0000

    Enable DynamoDB .tfstate Locking

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "votewave-tfstate-bucket"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "votewave-tf-locks"
     encrypt        = true
   }
 }
